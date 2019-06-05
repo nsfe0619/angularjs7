6c1a98427c613bec06a1083fdbe81b263c12dec8
nsfe <nsfe0619@gmail.com>
2019-06-05 11:12:52 +0800

    no message

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -2,18 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `<div>
-  test works!
-  <input id="{{myText}}"/>
-  <input [id]="myText"/>
-</div>`,
+  template: `
+  hello {{name}}!
+  <input id="{{myText}}" type="text" value="123"/>
+  <input [id]="myText" type="text" value="456"/>
+  <input [id]="myText" type="text" value="{{myValue}}"/>
+  <button (click)="btclick($event)">button</button>
+  {{greeting}}
+`,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
+  public name="Isaac";
   public myText="test";
+  public myValue="789";
+  public greeting="button message!!"
   constructor() { }
   ngOnInit() {
   }
-
+  btclick(event){
+    console.log('event',event);
+  }
 }
